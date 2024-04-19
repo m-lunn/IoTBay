bb42e5388aacc8f3beafe68b7e11ddd7c50e217c
Michael <michaellunn@git.edstem.org>
2024-04-19 18:38:39 +1000

    Clean

--- utils/bar.sh
+++ utils/bar.sh
@@ -1,6 +1,7 @@
-!#/bin/bash
+#!/bin/bash
 
 cd /Users/michaellunn/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/bin
+
 ./shutdown.sh
 
 /Library/Maven/bin/mvn package -f /Users/michaellunn/IoTBay/pom.xml
