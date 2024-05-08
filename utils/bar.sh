d44b0f516580d456429e891ffa70410638b6e184
Michael <michaellunn@git.edstem.org>
2024-05-08 15:25:13 +1000

    Edited build and restart script

--- utils/bar.sh
+++ utils/bar.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cd /Users/michaellunn/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/bin
+cd ~/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/bin
 
 ./shutdown.sh
 
-/Library/Maven/bin/mvn package -f /Users/michaellunn/IoTBay/pom.xml
+/Library/Maven/bin/mvn package -f ~/IoTBay/pom.xml
 
-mv /Users/michaellunn/IoTBay/target/IoTBay-1.0-SNAPSHOT.war /Users/michaellunn/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/webapps/ROOT.war
+mv ~/IoTBay/target/IoTBay-1.0-SNAPSHOT.war ~/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/webapps/ROOT.war
 
-cp /Users/michaellunn/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/webapps/ROOT.war /Users/michaellunn/IoTBay/target/
+cp ~/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/webapps/ROOT.war ~//IoTBay/target/
 
-mv /Users/michaellunn/IoTBay/target/ROOT.war /Users/michaellunn/IoTBay/target/IoTBay-1.0-SNAPSHOT.war
+mv ~/IoTBay/target/ROOT.war ~/IoTBay/target/IoTBay-1.0-SNAPSHOT.war
 
-rm -r /Users/michaellunn/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/webapps/ROOT
+rm -r ~/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/webapps/ROOT
 
 ./startup.sh
