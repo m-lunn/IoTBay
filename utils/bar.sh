98fd484bd2085427ce290f822e864a21f7edd1e7
Michael <michaellunn@git.edstem.org>
2024-05-19 13:01:23 +1000

    Added README

--- utils/bar.sh
+++ utils/bar.sh
@@ -6,11 +6,11 @@ cd ~/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4
 
 rm -r ~/IoTBay/target # Remove project target folder to prevent Maven caching fuckery.
 
-/Library/Maven/bin/mvn package -f ~/IoTBay/pom.xml # Package project into .war
+/Library/Maven/bin/mvn package -f ~/IoTBay/pom.xml # Package project into .war file
 
 rm -r ~/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/webapps/ROOT # Remove old deployment on server.
 
-mv ~/IoTBay/target/IoTBay-1.0.war ~/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/webapps/IoTBay-1.0.war # Overwrites old deployment with new deployment.
+mv ~/IoTBay/target/IoTBay-1.0.war ~/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-10.1.4/apache-tomcat-10.1.4/webapps/ROOT.war # Overwrites old deployment with new deployment.
 
 ./startup.sh # Starts the server.
 
